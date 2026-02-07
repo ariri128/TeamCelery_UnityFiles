8af788b05258d2816454b4ffc90dca0ea7323e52
Ray-128 <arya.v.kamat@gmail.com>
2026-02-07 16:31:31 -0500

    Commit VFX code

--- Assets/Scripts/CitronautTarget.cs
+++ Assets/Scripts/CitronautTarget.cs
@@ -4,6 +4,8 @@ public class CitronautTarget : MonoBehaviour, IShootableTarget
 {
     public CitronautSpawner spawner;
 
+    public GameObject explosionPrefab;
+
     public float escapeSpeed = 6f;
     public float destroyDelayWhenShot = 0.15f;
 
@@ -43,9 +45,20 @@ public class CitronautTarget : MonoBehaviour, IShootableTarget
         if (isEnding) return;
         isEnding = true;
 
+        // Hide citronaut instantly
+        SpriteRenderer sr = GetComponent<SpriteRenderer>();
+        if (sr != null) sr.enabled = false;
+
+        Collider2D col = GetComponent<Collider2D>();
+        if (col != null) col.enabled = false;
+
+        // Play explosion
+        SpawnExplosion();
+
         if (spawner != null)
             spawner.OnTargetFinished(true, transform.position.x);
 
+        // Remove object shortly after
         Destroy(gameObject, destroyDelayWhenShot);
     }
     
@@ -80,5 +93,36 @@ public class CitronautTarget : MonoBehaviour, IShootableTarget
             move.enabled = false;
 
         escaping = true;
+    }
+
+    private void SpawnExplosion()
+    {
+        if (explosionPrefab == null)
+        {
+            Debug.LogWarning("Explosion prefab not assigned on " + gameObject.name);
+            return;
+        }
+
+        // Spawn slightly in front (for 2D layering safety)
+        Vector3 pos = transform.position;
+        pos.z = -1f;
+
+        GameObject boom = Instantiate(explosionPrefab, pos, Quaternion.identity);
+
+        // Auto-destroy after particle finishes
+        ParticleSystem ps = boom.GetComponent<ParticleSystem>();
+        if (ps != null)
+        {
+            float time = ps.main.duration;
+
+            if (ps.main.startLifetime.mode == ParticleSystemCurveMode.Constant)
+                time += ps.main.startLifetime.constant;
+
+            Destroy(boom, time + 0.1f);
+        }
+        else
+        {
+            Destroy(boom, 2f);
+        }
     }
 }
