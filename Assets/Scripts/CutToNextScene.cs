dd5e98ca91d90d87fb07074983dad3a1ebf9cfb8
Ray-128 <arya.v.kamat@gmail.com>
2026-02-09 21:41:22 -0500

    Commit transition scene audio

---
+++ Assets/Scripts/CutToNextScene.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.SceneManagement;
+
+public class CutToNextScene : MonoBehaviour
+{
+    public PlayableDirector director; // drag Timeline object’s PlayableDirector here
+    public string nextSceneName = "Level2_Citronaut";
+
+    private bool loading = false;
+
+    void Awake()
+    {
+        if (director == null)
+            director = GetComponent<PlayableDirector>();
+    }
+
+    void OnEnable()
+    {
+        if (director != null)
+            director.stopped += OnTimelineStopped;
+    }
+
+    void OnDisable()
+    {
+        if (director != null)
+            director.stopped -= OnTimelineStopped;
+    }
+
+    void Start()
+    {
+        // If Play On Awake is OFF, you can force it here:
+        // if (director != null) director.Play();
+    }
+
+    void OnTimelineStopped(PlayableDirector d)
+    {
+        if (loading) return;
+        loading = true;
+
+        SceneManager.LoadScene(nextSceneName);
+    }
+}
